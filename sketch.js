ce6b1363b8d4045884374cf6e5f0cd884263e14e
Rico <muhai@gmx.de>
2022-06-28 21:01:01 +0200

    Change url

--- sketch.js
+++ sketch.js
@@ -34,7 +34,7 @@ function gotResults(error, results) {
   label = results[0].label;
   console.log(label)
   if(label === 'honey') {
-    window.location.href = 'https://chaosbohne.github.io/honey/sub/honey/index.html'
+    window.location.href = 'https://chaosbohne.github.io/honey/sub/game/index.html'
   }
 }
 
