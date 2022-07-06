274d60b5de5560092ed5ae53db4a617124be6b2a
Rico <muhai@gmx.de>
2022-07-06 23:25:37 +0200

    Nav

--- sketch.js
+++ sketch.js
@@ -39,13 +39,13 @@ function gotResults(error, results) {
   var confidence = results[0].confidence
 
   if(label === 'bee' && confidence > 0.85) {
-    window.location.assign('./sub/bee/index.html')
+    window.location.assign('https://chaosbohne.github.io/honey/sub/bee/index.html')
   }else if(label === 'honey' && confidence > 0.85) {
-    window.location.assign('./sub/honey/index.html')
+    window.location.assign('https://chaosbohne.github.io/honey/sub/honey/index.html')
   }else if(label === 'flower' && confidence > 0.85) {
-    window.location.assign('./sub/plant/index.html')
+    window.location.assign('https://chaosbohne.github.io/honey/sub/plant/index.html')
   }else if(label === 'glasses' && confidence > 0.85) {
-    window.location.assign('./sub/see/index.html')
+    window.location.assign('https://chaosbohne.github.io/honey/sub/see/index.html')
   }
 }
 
